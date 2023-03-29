587ef4bb99fdc9b67da1a5ed16f04b64c49c0fd8
MohitKumarMandhre <mohitmandhre@gmail.com>
2023-03-29 11:27:20 +0530

    datasets

---
+++ Venkat-assignments/DATASET/students.js
@@ -0,0 +1,4 @@
+db.students.insertOne({name: "Durga", rollno: 101, marks: 98 })
+db.students.insertOne({name: "Ravi", rollno: 102, marks: 99 })
+db.students.insertOne({name: "Shiva", rollno: 103, marks: 100 })
+db.students.insertOne({name: "Pavan", rollno: 104, marks: 80 })
